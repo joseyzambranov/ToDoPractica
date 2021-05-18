10d565c1b8d8df5d33014d43d315e0f9780c231c
jose zambrano <joseyzambranovpe@gmail.com>
2021-05-18 12:34:43 -0500

    quitando los log

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-console.log("Hello World")
-
 const formulario = document.getElementById('formulario')
 const listaTarea = document.getElementById('lista-tareas')
 const template = document.getElementById('template').content
 const fragmanet = document.createDocumentFragment()
 const input = document.getElementById('input')
-/*let tareas = {
-    1621344596196: {
-        id: 1621344596196,
-        texto: 'Tarea #1',
-        estado: false
-    },
-    1621344705424: {
-        id: 1621344705424,
-        texto: 'Tarea #2',
-        estado: false
-    }
-}*/
-let tareas={}
+
+let tareas = {}
 
 document.addEventListener('DOMContentLoaded', () => {
-    if(localStorage.getItem('tareas')){
+    if (localStorage.getItem('tareas')) {
         tareas = JSON.parse(localStorage.getItem('tareas'))
     }
     pintarTareas()
@@ -32,15 +19,13 @@ listaTarea.addEventListener('click', e => {
 
 formulario.addEventListener('submit', e => {
     e.preventDefault()
-    /*console.log(e.target[0].value)
-    console.log(e.target.querySelector('input').value)
-    console.log(input.value)*/
+
     setTareas(e)
 })
-//console.log(Date.now())
+
 const setTareas = e => {
     if (input.value.trim() === '') {
-        console.log('esta vacio')
+
         return
     }
     const tarea = {
@@ -49,7 +34,7 @@ const setTareas = e => {
         estado: false
     }
     tareas[tarea.id] = tarea
-    //console.log(tareas)
+
     formulario.reset()
     input.focus()
     pintarTareas()
@@ -57,9 +42,9 @@ const setTareas = e => {
 
 
 const pintarTareas = () => {
-    localStorage.setItem('tareas',JSON.stringify(tareas))
+    localStorage.setItem('tareas', JSON.stringify(tareas))
 
-    if(Object.values(tareas).length===0){
+    if (Object.values(tareas).length === 0) {
         listaTarea.innerHTML = `<div class="alert alert-dark text-center">
         No Hay Tareas Pendientes &#128525
     </div>`
@@ -85,24 +70,24 @@ const pintarTareas = () => {
 const btnAction = (e) => {
 
     if (e.target.classList.contains('fa-check-circle')) {
-        console.log(e.target.dataset.id)
+
         tareas[e.target.dataset.id].estado = true
         pintarTareas()
-        console.log(tareas)
+
 
     }
     if (e.target.classList.contains('fa-minus-circle')) {
         delete tareas[e.target.dataset.id]
         pintarTareas()
-        console.log(tareas)
+
     }
     if (e.target.classList.contains('fa-undo-alt')) {
         console.log(e.target.dataset.id)
         tareas[e.target.dataset.id].estado = false
         pintarTareas()
-        console.log(tareas)
+
 
     }
-    
+
     e.stopPropagation()
 }
